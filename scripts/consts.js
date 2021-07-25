dc3dafa16dd9bf3d0df9af090e5a0a777d2a1204
unknown <glappsmobile@gmail.com>
2021-07-25 19:10:12 -0300

    changed order of categories

--- scripts/consts.js
+++ scripts/consts.js
@@ -8,20 +8,21 @@ let arrOptions = [
     ],
 
     [
-        {name:"Pudim", id:"pudim", desc:"1 Fatia", price:"2,50"},
-        {name:"Salada de frutas", id:"salada-de-frutas", desc:"", price:"4,00"},
-        {name:"Brownie", id:"brownie", desc:"", price:"2,00"}, 
-        {name:"Sorvete", id:"sorvete", desc:"500ml", price:"6,00"}, 
-        {name:"Açaí", id:"acai", desc:"500ml", price:"7,00"}
-    ],
-
-   [
         {name:"Coca", id:"coca", desc:"300ml", price:"5,00"},
         {name:"Dolly", id:"dolly", desc:"300ml", price:"0,50"}, 
         {name:"Pepsi", id:"pepsi", desc:"300ml", price:"5,50"},
         {name:"Mate", id:"mate", desc:"300ml", price:"2,00"},
         {name:"Skol Beats", id:"skol-beats", desc:"300ml", price:"7,00"}
+    ],
+
+    [
+        {name:"Pudim", id:"pudim", desc:"1 Fatia", price:"2,50"},
+        {name:"Salada de frutas", id:"salada-de-frutas", desc:"", price:"4,00"},
+        {name:"Brownie", id:"brownie", desc:"", price:"2,00"}, 
+        {name:"Sorvete", id:"sorvete", desc:"500ml", price:"6,00"}, 
+        {name:"Açaí", id:"acai", desc:"500ml", price:"7,00"}
     ]
+
 ]
 
-let types = ["Prato", "Sobremesa", "Bebida"];
+let types = ["Prato", "Bebida", "Sobremesa"];
